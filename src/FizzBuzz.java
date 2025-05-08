793776a7d0d2689a743d359d397255d52f459cfa
rexho <rrexinc@gmail.com>
2025-05-08 19:51:53 -0400

    grg

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,33 +3,38 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
         int  i = 1;
         while (i <= 1000) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-            i ++;
+            i = wwwww(i);
         }
+    }
+
+    public static int wwwww(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
