1b471b96f96cff819d3e3c431f4b8ce0a2e4de65
rexho0 <rrexinc@gmail.com>
2025-05-08 19:47:19 -0400

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,7 @@ class FizzBuzz {
 
     public static void main(String[] args) {
         int  i = 1;
-        while (i <= 100) {
+        while (i <= 1000) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
